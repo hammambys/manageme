c9f5c084e61d3b3beaa393d4112f12fc0e296e96
hammam <hammambenyounes4@gmail.com>
2022-03-20 21:02:48 +0100

    .

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
-import {  NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { ClassComponent } from './views/class/class.component';
 import { GradesComponent } from './views/grades/grades.component';
 import { GroupsComponent } from './views/groups/groups.component';
 import { MessagesComponent } from './views/messages/messages.component';
 import { ScheduleComponent } from './views/schedule/schedule.component';
 import { SettingsComponent } from './views/settings/settings.component';
 
-
 const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'class', component: ClassComponent },
   { path: 'groups', component: GroupsComponent },
   { path: 'schedule', component: ScheduleComponent },
   { path: 'grades', component: GradesComponent },
@@ -19,6 +20,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
